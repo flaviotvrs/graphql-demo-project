1530558df0f5448d0901d481c916be818256f58b
Flávio Tavares <flaviotvrs@gmail.com>
2019-05-06 15:09:37 -0300

    fixing test configuration

--- src/test/java/br/com/flaviotvrs/graphql/GraphqlDemoProjectApplicationTests.java
+++ src/test/java/br/com/flaviotvrs/graphql/GraphqlDemoProjectApplicationTests.java
@@ -5,8 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import br.com.flaviotvrs.tutorial.GraphqlDemoProjectApplication;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = GraphqlDemoProjectApplication.class)
 public class GraphqlDemoProjectApplicationTests {
 
 	@Test
